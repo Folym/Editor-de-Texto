765bf6941a96557369385ceabf9901f7d09d79e0
Heitor Folim <94811303+Folym@users.noreply.github.com>
2022-10-03 11:24:23 -0300

    Update Editor.c

--- Editor.c
+++ Editor.c
@@ -32,6 +32,116 @@ void Aparencia(ListaLin **InicioPag ,PosicaoCur posCur)
 	//PALAVRA LISTAGEN
 }
 
+void LerArquivo(ListaLin **Linha,ListaCar **cursor,PosicaoCur *posCur, char nom[15])
+{
+	FILE *arq;
+	char car;
+	strcat(nom,".txt");
+	arq = fopen(nom,"r+");
+	if(arq==NULL)
+		printf("Arquivo não encontrado\n");
+	else
+	{
+		while(!feof(arq))
+		{
+			car = fgetc(arq);
+			if(car!='\n')
+				insereCar(&(*Linha),&(*(cursor)),car,&(*posCur));
+			else
+			{
+				insereProxL(&(*Linha));
+				(*Linha) = (*Linha)->botton;
+				*cursor = (*Linha)->inicioL;
+				(*posCur).col = 1;
+				(*posCur).lin++;
+			}
+		}
+	}
+	fclose(arq);
+}
+
+void SalvarArquivo(ListaLin *Linha,ListaCar **cursor,char nom[15])
+{
+	FILE *arq;
+	char car;
+	strcat(nom,".txt");
+	arq = fopen(nom,"w+");
+	while(Linha!=NULL)
+	{
+		(*cursor)= Linha->inicioL;
+		while((*cursor)->prox!=NULL)
+		{
+			fputc((*cursor)->letra,arq);
+			*cursor = (*cursor)->prox;
+		}
+		fputc('\n',arq);
+		Linha = Linha->botton;
+	}
+	fclose(arq);
+}
+
+void ExibirEscrito(ListaLin *Linha, ListaCar *cursor, int PL, int RE, int RD)
+{
+	clrscr();
+	char negt=0;
+	PosicaoCur posCur;
+	int count=0,i=0;
+	while(i<120)
+	{
+		printf("|");	
+		i++;
+	}
+	posCur.lin = 3;
+	posCur.col = PL;
+	while(Linha!=NULL)
+	{
+		cursor = Linha->inicioL;
+		while(cursor->prox!=NULL)
+		{
+			if(negt==1)
+				textcolor(RED);
+			else
+				textcolor(WHITE);
+			if(cursor->letra==27)
+				negt = !negt;
+			else
+			{
+				if(cursor->letra==46 || cursor->letra==63 || cursor->letra==33 || count>RD)
+				{
+					gotoxy(posCur.col,posCur.lin); printf("%c",cursor->letra);
+					posCur.col = PL;
+					posCur.lin++;
+					count=0;
+				}
+				else
+				{
+					gotoxy(posCur.col+RE,posCur.lin); printf("%c",cursor->letra);
+					posCur.col++;
+					count++;
+				}
+			}
+			cursor = cursor->prox;
+		}
+		Linha = Linha->botton;
+	}
+}
+
+void ConfParg(ListaLin *Linha, ListaCar *cursor)
+{
+	int PL,RE,RD;
+	clrscr();
+	gotoxy(2,2);	printf("Configuração dos parágrafos");
+	gotoxy(1,3);	printf("----------------------------");
+	gotoxy(2,6);	printf("Primeira Linha:");
+	gotoxy(2,8); 	printf("Recuo Esquerdo:");
+	gotoxy(2,10); 	printf("Recuo Direito:");
+	gotoxy(18,6);	scanf("%d",&PL);
+	gotoxy(18,8);	scanf("%d",&RE);
+	gotoxy(17,10);	scanf("%d",&RD);
+	ExibirEscrito(Linha,cursor,PL,RE,RD);
+	getch();
+}
+
 void Menu(ListaLin **InicioPag ,PosicaoCur posCur)
 {
 	clrscr();
@@ -43,7 +153,7 @@ void Digitar(ListaLin **Linhas, ListaLin **Linha, ListaCar **cursor)
 {
 	PosicaoCur posCur;
 	ListaLin *InicioPag = *Linhas;
-	char dig,negt=0;
+	char dig,arq[15];
 	posCur.lin = posCur.col = 1;
 	Menu(&InicioPag, posCur);
 	do
@@ -70,6 +180,18 @@ void Digitar(ListaLin **Linhas, ListaLin **Linha, ListaCar **cursor)
 						case 'M':
 							goRight(&(*Linha),&(*cursor),&posCur);
 							break;
+						case 71:
+							goHome(&(*Linha),&(*cursor),&posCur);
+							break;
+						case 73:
+							pageUp(InicioPag,&(*Linha),&(*cursor),&posCur);
+							break;
+						case 79:
+							goEnd(&(*Linha),&(*cursor),&posCur);
+							break;
+						case 81:
+							pageDown(InicioPag,&(*Linha),&(*cursor),&posCur);
+							break;
 						case 83:
 							deleteCar(&(*Linha),&(*cursor),&posCur);	
 							break;
@@ -79,10 +201,33 @@ void Digitar(ListaLin **Linhas, ListaLin **Linha, ListaCar **cursor)
 					dig = getch();
 					switch(dig)
 					{
+						case 60:
+							clrscr();
+							printf("Digite o nome do arquivo para abrir, sem o .txt\n");
+							gets(arq);
+							InicioPag = *Linhas;
+							*Linha = *Linhas;
+							clrscr();
+							LerArquivo(&(*Linha),&(*cursor),&posCur,arq);
+							break;
+						case 61:
+							clrscr();
+							printf("Digite o nome do arquivo que deseja salvar, sem o .txt\n");
+							gets(arq);
+							InicioPag = *Linhas;
+							*Linha = *Linhas;
+							clrscr();
+							SalvarArquivo(*Linha,&(*cursor),arq);
+							break;
 						case 62:
 							dig = 27;
+							break;
+						case 63:
+							*Linha = *Linhas;
+							ConfParg(*Linha,*cursor);
 						case 68:
 							insereCar(&(*Linha),&(*cursor),27,&posCur);
+							break;
 						
 					}
 					break;
@@ -104,13 +249,15 @@ void Digitar(ListaLin **Linhas, ListaLin **Linha, ListaCar **cursor)
 					break;
 				default:
 					insereCar(&(*Linha),&(*cursor),dig,&posCur);
-					break;	
+					break;
 			}
 			Menu(&InicioPag, posCur);
 		}
 	}
 	while(dig!=27);
 }
+
+
 int main(void)
 {
 	ListaLin *Linhas,*Linha;
@@ -119,14 +266,5 @@ int main(void)
 	insereFimL(&Linhas);
 	Linha = Linhas;
 	cursor = Linhas->inicioL;
-	Digitar(&Linhas,&Linha,&cursor);
-	
-
-	
-
-
-
-	
-	
-	
+	Digitar(&Linhas,&Linha,&cursor);	
 }
